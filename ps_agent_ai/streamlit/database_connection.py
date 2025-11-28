3f4b1c31bc9991267cd80b6bba3cb412d506f7a3
ajaykumar2014 <ajay.jamiahamdard@gmail.com>
2025-11-28 13:46:51 +0530

    Delete ps_agent_ai/streamlit directory

--- ps_agent_ai/streamlit/database_connection.py
+++
@@ -1,7 +0,0 @@
-
-import streamlit as st
-st.markdown("# Main page 🎈")
-st.sidebar.markdown("# Main page 🎈")
-conn = st.connection("bldTestVault")
-df = conn.query("select * from accountTable")
-st.dataframe(df)
